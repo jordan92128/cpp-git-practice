3ec4565a5358e5a26bf528c0c40ed44d9d53ba29
jordan92128 <jordan92128@gmail.com>
2026-08-26 11:32:51 -0700

    Add feature branch message

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout << "This line was added by Student B." << std::endl;
     std::cout << "Another change from Student A." << std::endl;
     std::cout << "I am online.Therefore, I am both student A and B" << std::endl;
+    std::cout << "Feature branch is working." << std::endl;
     return 0;
 }
