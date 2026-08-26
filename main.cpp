1db5fc4399d4ffbf4b2f17554f077845d77d1c83
jordan92128 <jordan92128@gmail.com>
2026-08-26 11:28:13 -0700

    Add 3rd message_ExtraPractice

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello from Git!" << std::endl;
     std::cout << "This line was added by Student B." << std::endl;
     std::cout << "Another change from Student A." << std::endl;
+    std::cout << "I am online.Therefore, I am both student A and B" << std::endl;
     return 0;
 }
